03cd2e003bd86572ded71c77d03528440b9c0c3c
sree <sreedevi1862004@gmail.com>
2026-04-22 17:56:49 +0530

    add whitenoise for static files

--- tiarrapro/tiarrapro/settings.py
+++ tiarrapro/tiarrapro/settings.py
@@ -68,6 +68,7 @@ SOCIALACCOUNT_PROVIDERS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -185,3 +186,5 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = False    # don't logout when browser closes
 CSRF_TRUSTED_ORIGINS = ['https://tiarra-production.up.railway.app']
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
